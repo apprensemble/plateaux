4cf92fb9fae5c5c429b983be8f6f836ecd4bcf89
DTSI Montpellier <thierry@apprensemble.link>
2019-06-12 14:46:10 +0200

    change to port 8080

--- app.js
+++ app.js
@@ -27,6 +27,6 @@ app.post('/abande', function (req, res) {
   res.send(message)
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
 })
