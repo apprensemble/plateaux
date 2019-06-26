9337496d14c3e92c630307c5b48aa6c10ae30041
DTSI Montpellier <thierry@apprensemble.link>
2019-06-26 15:05:08 +0200

    ajout de log

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
-  var message = ["inutile","Banana", "Orange", "Apple", "Mango"];
+  var message = ["inutile","Banana", "Orange", "Apple", "Mango"]
 
 app.post('/test_json', function (req, res, next) {
   console.log(req.body)
@@ -18,12 +18,14 @@ app.get('/', function (req, res) {
 for (i = 1; i < 5; i++) {
 
   app.get('/channel'+i, function (req, res) {
+  console.log(message[i])
   res.send(message[i])
 })
 
 app.post('/channel'+i, function (req, res) {
   console.log(req.body.message)
   message[i] = req.body.message
+  console.log(message[i])
   res.send(message[i])
 });
   
